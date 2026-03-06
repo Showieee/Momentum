15fdb308aff3a67902e5e7b756d7380a34ba3e28
Mircea BLENDEA <blendea.mircea1@gmail.com>
2026-03-06 20:47:17 +0200

    add validations

--- Momentum/Momentum.BackOffice/Features/Persons/PersonEntryViewModelValidator.cs
+++ Momentum/Momentum.BackOffice/Features/Persons/PersonEntryViewModelValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Momentum.BackOffice.Features.Persons;
 
@@ -10,8 +11,48 @@ public class PersonEntryViewModelValidatorValidator : AbstractValidator<PersonEn
 			.NotEmpty().WithMessage("First name is required.")
 			.Matches("^[a-zA-Z]*$").WithMessage("Only letter characters are allowed.");
 
-        RuleFor(x => x.LastName)
-            .NotEmpty().WithMessage("Last name is required.")
-            .Matches("^[a-zA-Z]*$").WithMessage("Only letter characters are allowed.");
-    }
+		RuleFor(x => x.LastName)
+			.NotEmpty().WithMessage("Last name is required.")
+			.Matches("^[a-zA-Z]*$").WithMessage("Only letter characters are allowed.");
+
+		RuleFor(x => x.CNP)
+			.NotEmpty().WithMessage("CNP is required.");
+
+		RuleFor(x => x.Address!.StreetName)
+			.NotEmpty().WithMessage("Street name is required.")
+			.When(x => x.Address != null && !IsEmptyAddress(x.Address));
+
+		RuleFor(x => x.Address!.StreetNumber)
+			.NotEmpty().WithMessage("Street number is required.")
+			.When(x => x.Address != null && !IsEmptyAddress(x.Address));
+
+		RuleFor(x => x.Address!.City)
+			.NotEmpty().WithMessage("City is required.")
+			.When(x => x.Address != null && !IsEmptyAddress(x.Address));
+
+		RuleFor(x => x.Address!.State)
+			.NotEmpty().WithMessage("State is required.")
+			.When(x => x.Address != null && !IsEmptyAddress(x.Address));
+
+		RuleFor(x => x.Address!.Country)
+			.NotEmpty().WithMessage("Country is required.")
+			.When(x => x.Address != null && !IsEmptyAddress(x.Address));
+
+		RuleFor(x => x.Address!.Email)
+			.NotEmpty().WithMessage("Email is required.")
+			.EmailAddress().WithMessage("Invalid email address.")
+			.When(x => x.Address != null && !IsEmptyAddress(x.Address));
+
+	}
+
+	private static bool IsEmptyAddress(AddressEntryViewModel address)
+	{
+		return address.StreetName.IsNullOrEmpty() &&
+			   address.StreetNumber.IsNullOrEmpty() &&
+			   address.City.IsNullOrEmpty() &&
+			   address.State.IsNullOrEmpty() &&
+			   address.Country.IsNullOrEmpty() &&
+			   address.Email.IsNullOrEmpty();
+	}
 }
+
